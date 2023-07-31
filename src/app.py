9dde5ae04da7b8fbb02f33e2167df96e02aa74c6
Marcos Lopes <marcos.mpdcl@gmail.com>
2023-07-30 22:36:34 -0400

    fix: Corringindo questões .env

--- src/app.py
+++ src/app.py
@@ -9,13 +9,13 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['ERROR_INCLUDE_MESSAGE'] = os.environ.get('ERROR_INCLUDE_MESSAGE')
+app.config['ERROR_INCLUDE_MESSAGE'] = os.environ.get('ERROR_INCLUDE_MESSAGE') == 'True'
 api.init_app(app)
 api.add_namespace(ns)
 
 if __name__ == '__main__':
     app.run(
-        debug = os.environ.get('DEBUG'), 
+        debug = os.environ.get('DEBUG') == 'True', 
         host = os.environ.get('HOST'), 
-        port = os.environ.get('PORT')
+        port = int(os.environ.get('PORT'))
         )
